7df7ab0297a8173e02617517bd3dc487f43ba483
bronstedk <djieumoraphael@gmail.com>
2025-08-27 01:30:21 -0400

    darwin/system: expand macOS defaults and settings
    - Disable startup chime
    - Configure login window to disable guest
    - Extend Dock options (hot corners, hide/show, recents)
    - Add Finder defaults (path in title, show extensions/files, desktop visibility, etc.)
    - Customize NSGlobalDomain (spelling, quotes, capitalization, dark mode, hidden menu bar)
    - Enable window manager grouping behavior

--- darwin/modules/system.nix
+++ darwin/modules/system.nix
@@ -1,22 +1,65 @@
 { ... }: {
 	time.timeZone = "America/New_York";
 
-	system.defaults = {
-		controlcenter = {
-			Sound = false;
-			NowPlaying = false;
-			FocusModes = false;
-			Display = false;
-			Bluetooth = false;
-			AirDrop = false;
+	system = {
+		startup.chime = false; 
 
-			BatteryShowPercentage = true;
-		};
+		defaults = {
+			hitoolbox.AppleFnUsageType = "Start Dictation";
 
-		dock = {
-			persistent-apps = [];
+			loginwindow.GuestEnabled = false;
 
+			controlcenter = {
+				Sound = false;
+				NowPlaying = false;
+				FocusModes = false;
+				Display = false;
+				Bluetooth = false;
+				AirDrop = false;
 
+				BatteryShowPercentage = true;
+			};
+
+			dock = {
+				persistent-apps = [
+				];
+
+				wvous-tr-corner = 1;
+				wvous-tl-corner = 1;
+				wvous-br-corner = 11;
+				wvous-bl-corner = 2;
+
+				showhidden = true;
+				show-recents = false;
+			};
+
+			finder = {
+				_FXShowPosixPathInTitle = true;
+				_FXSortFoldersFirst = true;
+				AppleShowAllExtensions = true;
+				AppleShowAllFiles = true;
+				CreateDesktop = false;
+				FXDefaultSearchScope = "SCcf";
+				FXEnableExtensionChangeWarning = true;
+				FXPreferredViewStyle = "Nlsv";
+				FXRemoveOldTrashItems = true;
+				NewWindowTarget = "Home";
+				ShowPathbar = true;
+				ShowStatusBar = true;
+			};
+
+			NSGlobalDomain = {
+				NSAutomaticSpellingCorrectionEnabled = false;
+				NSAutomaticQuoteSubstitutionEnabled = false;
+				NSAutomaticPeriodSubstitutionEnabled = false;
+				NSAutomaticCapitalizationEnabled = false;
+				AppleShowAllFiles = true;
+				AppleShowAllExtensions = true;
+				AppleInterfaceStyle = "Dark";
+				_HIHideMenuBar = true;
+			};
+
+			WindowManager.AppWindowGroupingBehavior = true;
 		};
 	};
 }
